4713fad9fd0c7bc916d997a1b82983772c59a528
cagtay <cagtay.bayrak@ideateknoloji.com.tr>
2021-04-17 19:35:37 +0300

    Bootstrap

--- MVC5-Otomasyon/MVC5-Otomasyon/App_Start/BundleConfig.cs
+++ MVC5-Otomasyon/MVC5-Otomasyon/App_Start/BundleConfig.cs
@@ -19,12 +19,27 @@ namespace MVC5_Otomasyon
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+
+
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+   "~/Scripts/bootstrap.js",
+   "~/Scripts/bootstrap.min.js",
+   "~/Scripts/moment.min.js",
+   "~/Scripts/bootstrap-sortable.js",
+   "~/Scripts/respond.js"));
+
+
+
+
+
+
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+   "~/Content/bootstrap.min.css",
+   "~/Content/bootstrap-sortable.css",
+   "~/Content/site.css"));
+
+
         }
     }
 }
